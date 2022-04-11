454ecb92af1f7c45e08351380a254117f6550a10
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-04-11 13:27:11 -0500

    use linq and SequenceEqual method

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+
+        public bool IsAPalindrome(string str)
+        {
+            return str.SequenceEqual(str.Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("something", false)]
+        [InlineData("civic", true)]
+        [InlineData("kayak", true)]
+        [InlineData("madam", true)]
+        public void Test1(string word, bool expected)
         {
+            //Arrange
+            WordSmith challenger = new WordSmith();
 
+            //Act
+            bool actual = challenger.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
